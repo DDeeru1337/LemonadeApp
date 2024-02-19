8a24d9545b21dbede19405e21bafa265b40b2cd4
DDeer <ddeeru1337@gmail.com>
2024-02-19 19:54:43 +0300

    Ещё немного доделать приложение!

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,9 +25,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,23 +68,30 @@ fun LemonadeImageAndText(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Image(
-            painter = painterResource(id = imageResource),
-            contentDescription = counter.toString(),
+        Box(
             modifier = Modifier
+                .background(Color.LightGray)
                 .clickable {
-                    if (counter == 2) {
-                        when(randomNumber) {
-                            5 -> counter++
-                            else -> randomNumber++
-                        }
-                    } else if (counter == 4) {
-                        counter = 1
-                    } else {counter++}
+            if (counter == 2) {
+                when(randomNumber) {
+                    5 -> counter++
+                    else -> randomNumber++
                 }
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = userText)
+            } else if (counter == 4) {
+                counter = 1
+            } else {counter++}
+        }
+        ) {
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = counter.toString()
+            )
+        }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = userText,
+                fontSize = 24.sp
+                )
     }
 }
 
