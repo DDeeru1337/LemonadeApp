86f85bd19dc7fd19fba4f45f51efc66a88b9cc33
DDeer <ddeeru1337@gmail.com>
2024-02-19 17:36:17 +0300

    Доделать приложение!

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-
+                LemonadeApp()
             }
         }
     }
@@ -25,11 +39,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonadeImageAndText(modifier: Modifier = Modifier) {
-
+    var counter by remember {
+        mutableStateOf(1)
+    }
+    var randomNumber = (2..4).random()
+    val imageResource = when(counter) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val userText = when(counter) {
+        1 -> "Tap the lemon tree to select a lemon"
+        2 -> "Keep tapping the lemon to squeeze it"
+        3 -> "Tap the lemonade to drink it"
+        else -> "Tap the empty glass to start again"
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = counter.toString(),
+            modifier = Modifier
+                .clickable {
+                    if (counter == 2) {
+                        when(randomNumber) {
+                            5 -> counter++
+                            else -> randomNumber++
+                        }
+                    } else if (counter == 4) {
+                        counter = 1
+                    } else {counter++}
+                }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = userText)
+    }
 }
 
 @Preview
 @Composable
 fun LemonadeApp() {
-    LemonadeImageAndText()
+    LemonadeImageAndText(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
